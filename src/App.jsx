bf018a838d76c8530dc5bf0c8a9e0c2c65a94b9a
RemcoKuipers <remslaap@live.nl>
2026-02-23 13:47:26 +0100

    Gestart met huiswerk les 12 Opdracht 1 en 2 voltooid

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
 import './App.css'
 import {useState} from "react";
+import {useForm} from "react-hook-form";
 
 function App() {
+    const {register} = useForm();
     const [strawberries, setStrawberries] = useState(0)
     const [bananas, setBananas] = useState(0)
     const [apples, setApples] = useState(0)
     const [kiwis, setKiwis] = useState(0)
 
-    function resetCounters(){
+    function resetCounters() {
         setStrawberries(0);
         setApples(0);
         setBananas(0);
         setKiwis(0);
     }
+function handleSubmit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const data={
+            firstName: formData.get("firstName"),
+            lastName: formData.get("lastName"),
+            age: formData.get("age"),
+            zipcode: formData.get("zipcode"),
+            deliverFrequency: formData.get("deliverFrequency"),
+            choice: formData.get("choice"),
+            suggestions: formData.get("suggestion"),
+            terms: formData.get("terms"),
+
+            fruits:{
+                strawberries,
+                bananas,
+                apples,
+                kiwis,
+            },
+        };
+        console.log(data);
+}
 
     return (
 
@@ -50,6 +76,60 @@ function App() {
                 <article>
                     <button type="button" onClick={() => resetCounters()}>Reset</button>
                 </article>
+            </section>
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="firstName-field">
+                        Voornaam
+                        <input
+                            type="text"
+                            id="firstName-field"
+                            name="firstName"
+                        />
+                    </label>
+                    <label htmlFor="lastName-field">
+                        Achternaam
+                        <input type="text"
+                        id="lastName-field"
+                        name="lastName"/>
+                    </label>
+                    <label htmlFor="age-field">
+                        Leeftijd
+                        <input type="number"
+                        id="age-field"
+                        name="age"/>
+                    </label>
+                    <label htmlFor="zipcode-field">
+                        Postcode
+                        <input type="text"
+                        id="zipcode-field"
+                        name="zipcode"/>
+                    </label>
+                    <label htmlFor="deliverFrequenty-field">
+                        Bezorgrequentie
+                        <select name="deliverFrequenty" id="deliverFrequenty-field">
+                            <option value="every week">iedere week</option>
+                            <option value="every 2 weeks">om de week</option>
+                            <option value="every month">iedere maand</option>
+                        </select>
+                    </label>
+                    <label htmlFor="timeSlot-field">
+                        <input type="radio" id="dayTime" name="choice" value="Overdag" required />
+                        Overdag
+                    </label>
+                    <label htmlFor="timeSlot-field">
+                        <input type="radio" id="evening" name="choice" value="'s Avonds" />
+                        's Avonds
+                    </label>
+                    <label htmlFor="suggestions-field">
+                        Opmerking
+                        <textarea name="suggestion" id="suggestion-field" cols="30" rows="6"></textarea>
+                    </label>
+                    <label htmlFor="terms-field">
+                        <input type="checkbox" id="terms-field" name="terms" required /> Ik ga akkoord met de voorwaarden
+                    </label>
+                    <button type={"submit"}>Verzend</button>
+                </form>
             </section>
 
         </>
